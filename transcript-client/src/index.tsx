dc95146c44c6a2624bdfb3c5dbe568a86f0d8bb9
Lexi Cobb <lexicobb42@gmail.com>
2023-11-16 14:11:05 -0600

    handle file upload errors

--- transcript-client/src/index.tsx
+++ transcript-client/src/index.tsx
@@ -4,6 +4,7 @@ import { ChakraProvider } from "@chakra-ui/react";
 import App from 'src/App';
 import theme from 'src/theme/theme';
 import { TranscriptionProvider } from 'src/context/TranscriptionContext';
+import { ToastContainer } from 'react-toastify';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,5 +16,6 @@ root.render(
         <App />
       </TranscriptionProvider>
     </ChakraProvider>
+    <ToastContainer />
   </React.StrictMode>,
 )
