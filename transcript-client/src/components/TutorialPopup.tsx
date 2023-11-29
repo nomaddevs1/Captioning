2cd0f96d1df77060d0bad37b2f374670768b46f9
nickingenito <ningenito@gmail.com>
2023-11-29 10:51:32 -0600

    Update tutorial toggle

--- transcript-client/src/components/TutorialPopup.tsx
+++ transcript-client/src/components/TutorialPopup.tsx
@@ -2,19 +2,18 @@ import { Box, Text, Button } from "@chakra-ui/react";
 import { useState } from 'react';
 
 function TutorialPopup (props: any) {
-    let toggle = true;
-    const [showTutorial, setShowTutorial] = useState(toggle);
-    const toggleTutorial = () => setShowTutorial(toggle ? false : true);
-
+    const [showTutorial, setShowTutorial] = useState(false);
+    
     return (
         <Box pos="fixed" bottom="4" right="4">
             { showTutorial ?
-            <Button bg="none" border="solid" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" borderWidth="3px" onClick={toggleTutorial}>?</Button> :
-            <Box width="400px" bg="white" p="2" borderRadius="10">
-                <Box textAlign="left" fontSize="md">
-                    <Text>{props.text}</Text>
-                </Box>
-            </Box>
+                <Box width="400px" bg="white" p="2" borderRadius="10">
+                    <Box textAlign="left" fontSize="md">
+                        <Box as="button" pos="absolute" top="0" right="2" opacity="60%" _hover={{ opacity: "100%" }} onClick={() => setShowTutorial(false)}>x</Box>
+                        <Text>{props.text}</Text>
+                    </Box>
+                </Box>:
+                <Button bg="none" border="solid 3px" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" onClick={() => setShowTutorial(true)}>?</Button>
             }
         </Box>
     )
