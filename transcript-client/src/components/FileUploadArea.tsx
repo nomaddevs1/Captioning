066d50794b0b4cd746c79875fa115e62faade586
nickingenito <ningenito@gmail.com>
2023-11-29 12:23:12 -0600

    Update tutorial design and toggle

--- transcript-client/src/components/FileUploadArea.tsx
+++ transcript-client/src/components/FileUploadArea.tsx
@@ -18,10 +18,15 @@ const FileUploadArea = ({ getInputProps, getRootProps } :UploadProps) => (
       <Text mt={4} mb={1}>Drag and drop file here, or</Text>
       <Box>
         <input {...getInputProps()} />
-        <Text fontSize="md" mb={4}>choose a file to upload</Text>
+        <Text cursor="pointer" fontSize="md" mb={4}>choose a file to upload</Text>
       </Box>
     </Flex>
-    <TutorialPopup text={"Test tutorial text."} />
+    <TutorialPopup text={
+      <Box>
+        <Text mb="2">Welcome to Captioner, the assistive tool to generate transcriptions of audio files with multi-language support.</Text>
+        <Text>Upload a podcast or interview in a variety of formats (mp3, mp4, mpeg, mpga, mp4a, wav, webm) and follow the prompts to display the transcript.</Text>
+      </Box>
+    } />
   </Box>
 );
 
--- transcript-client/src/components/TutorialPopup.tsx
+++ transcript-client/src/components/TutorialPopup.tsx
@@ -1,4 +1,5 @@
-import { Box, Text, Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
+
 import { useState } from 'react';
 
 function TutorialPopup (props: any) {
@@ -6,17 +7,18 @@ function TutorialPopup (props: any) {
     
     return (
         <Box pos="fixed" bottom="4" right="4">
-            { showTutorial ?
-                <Box width="400px" bg="white" p="2" borderRadius="10">
-                    <Box textAlign="left" fontSize="md">
-                        <Box as="button" pos="absolute" top="0" right="2" opacity="60%" _hover={{ opacity: "100%" }} onClick={() => setShowTutorial(false)}>x</Box>
-                        <Text>{props.text}</Text>
+            { showTutorial ? 
+                <Box>
+                    <Box width="400px" bg="white" borderRadius="8" mb="4">
+                        <Box height="8px" bg="#557E4A" borderTopRadius="8"></Box>
+                        <Box p="2" textAlign="left" fontSize="md">{props.text}</Box>
                     </Box>
-                </Box>:
-                <Button bg="none" border="solid 3px" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" onClick={() => setShowTutorial(true)}>?</Button>
+                    <Button bg="none" border="solid 3px" float="right" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" width="56px" height="56px" onClick={() => setShowTutorial(false)}>X</Button>
+                </Box> :
+                <Button bg="none" border="solid 3px" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" width="56px" height="56px" onClick={() => setShowTutorial(true)}>?</Button>
             }
         </Box>
-    )
+    );
 }
 
 export default TutorialPopup
