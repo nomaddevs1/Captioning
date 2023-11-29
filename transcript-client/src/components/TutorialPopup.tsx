d9c92768af8faf1e4b41c82a738e589eff4bed01
nickingenito <ningenito@gmail.com>
2023-11-29 12:59:22 -0600

    Cleanup style props for buttons

--- transcript-client/src/components/TutorialPopup.tsx
+++ transcript-client/src/components/TutorialPopup.tsx
@@ -1,8 +1,18 @@
 import { Box, Button } from "@chakra-ui/react";
-
 import { useState } from 'react';
 
 function TutorialPopup (props: any) {
+    const buttonStyle = {
+        bg: 'none',
+        border: 'solid 3px',
+        float: 'right',
+        borderRadius: '50%',
+        borderColor: "#557E4A",
+        fontSize: 'xlg',
+        width: '56px',
+        height: '56px'
+    };
+
     const [showTutorial, setShowTutorial] = useState(false);
     
     return (
@@ -13,9 +23,9 @@ function TutorialPopup (props: any) {
                         <Box height="8px" bg="#557E4A" borderTopRadius="8"></Box>
                         <Box p="2" textAlign="left" fontSize="md">{props.text}</Box>
                     </Box>
-                    <Button bg="none" border="solid 3px" float="right" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" width="56px" height="56px" onClick={() => setShowTutorial(false)}>X</Button>
+                    <Button sx={buttonStyle} onClick={() => setShowTutorial(false)}>X</Button>
                 </Box> :
-                <Button bg="none" border="solid 3px" borderRadius="50%" fontSize="xlg" borderColor="#557E4A" width="56px" height="56px" onClick={() => setShowTutorial(true)}>?</Button>
+                <Button sx={buttonStyle} onClick={() => setShowTutorial(true)}>?</Button>
             }
         </Box>
     );
