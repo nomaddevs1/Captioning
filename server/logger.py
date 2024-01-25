4cdf228d18bc4bcc36dfa1fc321c057a1cfcfe96
Bell, Ryan <ryan.bell62101@gmail.com>
2024-01-24 18:52:13 -0600

    added rotating file logs and log statements throughout the server codebase.

---
+++ server/logger.py
@@ -0,0 +1,13 @@
+import logging
+from logging import StreamHandler
+from logging.handlers import RotatingFileHandler
+import sys
+
+
+def init_logger(log_file: str = None):
+    logging.getLogger().setLevel(logging.DEBUG)
+    if log_file != None:
+        logging.getLogger().addHandler(
+            RotatingFileHandler(filename=log_file, maxBytes=1000, backupCount=5)
+        )
+        logging.getLogger().addHandler(StreamHandler(sys.stdout))
