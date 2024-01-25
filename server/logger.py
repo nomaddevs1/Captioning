f41d67e9677fd1e7ceb4ebda7592f26f099c0adf
Bell, Ryan <ryan.bell62101@gmail.com>
2024-01-24 20:50:58 -0600

    forgot to run the linter

--- server/logger.py
+++ server/logger.py
@@ -8,6 +8,8 @@ def init_logger(log_file: str = None):
     logging.getLogger().setLevel(logging.DEBUG)
     if log_file != None:
         logging.getLogger().addHandler(
-            RotatingFileHandler(filename=log_file, maxBytes=10*1024*1024, backupCount=5)
+            RotatingFileHandler(
+                filename=log_file, maxBytes=10 * 1024 * 1024, backupCount=5
+            )
         )
         logging.getLogger().addHandler(StreamHandler(sys.stdout))
