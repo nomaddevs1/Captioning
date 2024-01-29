872bf9ec8755fa82806b4ef26e64ef15e0735618
nickingenito <ningenito@gmail.com>
2024-01-28 22:24:50 -0600

    Add index as key

--- transcript-client/src/components/TutorialPopup.tsx
+++ transcript-client/src/components/TutorialPopup.tsx
@@ -30,7 +30,7 @@ const TutorialPopup: React.FC<TutorialPopupProps> = ({tutorials}) => {
     let tutorial_list: Array<JSX.Element> = [];
     for (let i = 0; i < tutorials.length; i++){
         tutorial_list.push(
-            <Box sx={tutorials[i].position} width="350px" bg="white" borderRadius="8" mb="4" zIndex="1000">
+            <Box key={i} sx={tutorials[i].position} width="350px" bg="white" borderRadius="8" mb="4" zIndex="1000">
                 <Box height="8px" bg="#557E4A" borderTopRadius="8"></Box>
                 <Box p="2" textAlign="left" fontSize="md">
                     <Text>{tutorials[i].text}</Text>
