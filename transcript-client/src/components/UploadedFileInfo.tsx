1054cf230cbd5c8a1d86481890ff66dc660e9c43
JAGluna <javiergutierrezluna11@gmail.com>
2024-02-02 14:30:55 -0600

    added the supported languages by whisperAI to the drop down menu
    Co-authored-by: farouq <nomaddevs1@users.noreply.github.com>

--- transcript-client/src/components/UploadedFileInfo.tsx
+++ transcript-client/src/components/UploadedFileInfo.tsx
@@ -1,4 +1,4 @@
-import  { ReactNode } from 'react';
+import { useState, ReactNode } from 'react';
 import TutorialPopup from "src/components/TutorialPopup";
 import { Box, Text, Flex, Select } from "@chakra-ui/react";
 //@ts-ignore
@@ -6,35 +6,62 @@ import upload_logo from '../assets/upload_logo.svg';
 
 interface UploadedFileInfoProps {
   file: File;
-  children?: ReactNode; 
+  children?: ReactNode;
+  onChange: (language: string) => void;
 }
 
-const UploadedFileInfo= ({ file, children }: UploadedFileInfoProps) => (
+const UploadedFileInfo = ({ file, children, onChange }: UploadedFileInfoProps) => {
+  const [selectedLanguage, setSelectedLanguage] = useState(''); // State for selected language
+
+  // Function to handle language selection
+  const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const newLanguage = event.target.value;
+    setSelectedLanguage(newLanguage);
+    onChange(newLanguage); // Call the passed onChange function with the new language
+    
+  };
+
+  return (
   <Box>
     <Flex mb={4}>
       <img src={upload_logo} width="80px" alt="" />
       <Box ml={4}>
         <Text fontSize="3xl" mb={2} mr={0}>{file.name}</Text>
         <Flex alignItems="center">
-          <Select placeholder="Language" width="68%" bg="primary.moss.100">
-            <option value='english'>English</option>
+          <Select placeholder="Language" width="68%" bg="primary.moss.100" value={selectedLanguage} onChange={handleLanguageChange}>
+            <option value='da'>Danish</option>
+            <option value='nl'>Dutch</option>
+            <option value='en'>English</option>
+            <option value='fr'>French</option>
+            <option value='de'>German</option>
+            <option value='ja'>Japanese</option>
+            <option value='no'>Norwegia n</option>
+            <option value='pl'>Polish</option>
+            <option value='pt'>Portuguese</option>
+            <option value='es'>Spanish</option>
+            <option value='sv'>Swedish</option>
+            <option value='th'>Thai</option>
+            <option value='uk'>Ukrainian</option>
+            <option value='vi'>Vietnamese</option>
+
           </Select>
           <Text width="auto" ml={2}>{Math.round(file.size / 1000000)} MB</Text>
         </Flex>
       </Box>
     </Flex>
     {children} {/* Render children here */}
-    <TutorialPopup 
-      position={{pos: "fixed", bottom: "20", right: "4"}}
+    <TutorialPopup
+      position={{ pos: "fixed", bottom: "20", right: "4" }}
       text={
         <Box>
           <Text>
             Now that the file has been uploaded, select the transcript language from the dropdown menu and click 'Transcribe'.
           </Text>
         </Box>
-      } 
+      }
     />
   </Box>
-);
+  );
+    };
 
 export default UploadedFileInfo;
