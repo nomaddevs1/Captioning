667adbd40052828224e9c738e9596bfcfd5b44cd
MaxPetrutsa <maxpetrutsa@gmail.com>
2024-02-04 15:01:36 -0600

    Added download functionality

--- transcript-client/src/components/sidebar/transcriptionItem.tsx
+++ transcript-client/src/components/sidebar/transcriptionItem.tsx
@@ -1,5 +1,8 @@
 import { Box, Button, Text , Grid} from "@chakra-ui/react"
 import { ReactNode } from "react"
+import useDownloader from "src/hooks/useDownloader"
+
+
 
 interface TranscriptionItemProps {
   title: string
@@ -7,13 +10,33 @@ interface TranscriptionItemProps {
 }
 
 const TranscriptionBarItem = ({ title, children }: TranscriptionItemProps) => {
+  const { generatePDF, isLoading } = useDownloader();
+
+  const handleDownloadPDF = async () => {
+    try {
+      const blob = await generatePDF();
+
+      // Initiate the download
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement('a');
+      a.href = url;
+      a.download = 'generated-pdf.pdf';
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+    } catch (error) {
+      // Handle errors if needed
+      console.error('Error downloading transcript:', error);
+    }
+  };
+  
   return (
     <Box mb="20px">
       <Text mb="20px" width="100%" fontSize="1.2625rem" fontWeight="800" color={"neutral.50"}>
         {title}
       </Text>
       {children}
-        <Button mt="10px" width="100%" mb="70px" color="black">Save Transcript</Button>
+        <Button mt="10px" width="100%" mb="70px" color="black" onClick={handleDownloadPDF} disabled={isLoading}>{isLoading ? 'Generating PDF...' : 'Save Transcript'}</Button>
     </Box>
   )
 }
