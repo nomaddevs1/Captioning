168a03972385fb3953a1aff63adaa073cf54fc5d
nickingenito <ningenito@gmail.com>
2024-02-11 12:49:35 -0600

    Center tutorials on mobile displays

--- transcript-client/src/utils/standardTutorials.ts
+++ transcript-client/src/utils/standardTutorials.ts
@@ -1,30 +1,30 @@
 export const standardTutorials = [
   {
-    position: {pos: "fixed", right: "100", top: "180"},
+    position: {pos: "fixed", right: {mb: "100"}, top: {base: "130px", md: "180"}},
     text: "Now you will see the transcript of your audio file in this field, you are able to edit the text directly to fix any mistakes in the transcription process."
   },
   {
-    position: {pos: "fixed", left: "380", top: "250"},
+    position: {pos: "fixed", left: {md: "380"}, top: {base: "130px", md: "250"}},
     text: "Use the sidebar to adjust the font style and size of the transcript text, or adjust the spacing between lines and words."
   },
   {
-    position: {pos: "fixed", left: "380", top: "390"},
+    position: {pos: "fixed", left: {md: "380"}, top: {base: "130px", md: "390"}},
     text: "Select lines within the text display to bold, italicize, or underline them."
   },
   {
-    position: {pos: "fixed", left: "380", top: "550"},
+    position: {pos: "fixed", left: {md: "380"}, top: {base: "130px", md: "550"}},
     text: "Adjust the color of the whole text or select lines to highlight specific sections."
   },
   {
-    position: {pos: "fixed", left: "380", bottom: "1"},
+    position: {pos: "fixed", left: {md: "380"}, top: {base: "130px", md: "90%"}},
     text: "When you are done customizing the text, click 'Save Transcript' to download a pdf file to your computer with your chosen accessibility settings."
   },
   {
-    position: {pos: "fixed", left: "280", top: "160px"},
+    position: {pos: "fixed", left: {md: "280"}, top: {base: "130px", md: "160px"}},
     text: "If you would like to reset your changes to the default settings click on 'Reset Editor'."
   },
   {
-    position: {pos: "fixed", left: "480", top: "160px"},
+    position: {pos: "fixed", left: {md: "480"}, top: {base: "130px", md: "160px"}},
     text: "Click on 'Switch to Interactive View' to switch to an interactive experience with media controls that will highlight text as is it spoken in your uploaded audio file."
   }
 ]
