999b3f743c5d487e9b850fa5ab82b7c5196f83fc
nickingenito <ningenito@gmail.com>
2024-02-16 10:26:31 -0600

    Replace client URL with localhost instead of IP address

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
 
 # Fetching URLs from environment variables or setting default values
 ADDRESS = os.getenv("APP_ADDRESS", "http://127.0.0.1")
-CLIENT_URL = os.getenv("APP_CLIENT_URL", "http://127.0.0.1:3000")
+CLIENT_URL = os.getenv("APP_CLIENT_URL", "http://localhost:3000")
 PORT = os.getenv("APP_PORT", "8000")
 HOST_URL = f"{ADDRESS}:{PORT}"
 LOG_FILE = os.getenv("APP_LOG_FILE")
