78cf8ec7293ed855a0a4acff454a8dc384253022
Bell, Ryan <ryan.bell62101@gmail.com>
2024-02-22 21:40:06 -0600

    added environment variable for api url to workflow

--- transcript-client/src/utils/axios.ts
+++ transcript-client/src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = process.env.PROD ? "/api" : "http://localhost:8000"
+export const API_URL = process.env.PROD ? "https://api.transcribro.com" : "http://localhost:8000"
 
 export const AxiosPrivateClient = axios.create({
   baseURL: API_URL,
