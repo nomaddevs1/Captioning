871d63ecb8205463afc2cfb46a7b06944c19ff4f
nomaddevs1 <farouqsiwoku@gmail.com>
2024-03-02 17:12:04 -0600

    Fixed the language generating issue

--- server/pdf_generator/generator.py
+++ server/pdf_generator/generator.py
@@ -1,19 +1,19 @@
-from jinja2 import Environment, PackageLoader, select_autoescape
-import pdfkit
 import logging
+import pdfkit
+from jinja2 import Environment, PackageLoader, select_autoescape
 
 env = Environment(loader=PackageLoader("pdf_generator"), autoescape=select_autoescape())
 
 
 def render_html(data: dict) -> str:
-    logging.info("rendering HTML document to convert to PDF file...")
+    logging.info("rendering html document to convert to pdf file...")
     template = env.get_template("transcript.html")
     html_str = template.render(**data)
     return html_str
 
 
 def generate_pdf(html_data: str):
-    logging.info("generating PDF file...")
+    logging.info("generating pdf file...")
     return pdfkit.from_string(
         html_data,
         False,
@@ -25,5 +25,6 @@ def generate_pdf(html_data: str):
             "enable-javascript": True,
             "javascript-delay": 1000,  # may need to adjust the delay
             "no-stop-slow-scripts": True,
+            "encoding": "UTF-8",
         },
     )
