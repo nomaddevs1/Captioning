a1df1a62e15b9bffa62cdb1f7bc6b64b4b92e101
nomaddevs1 <farouqsiwoku@gmail.com>
2024-03-04 05:38:07 -0600

    fixing testing

---
+++ transcript-client/src/hooks/.null-ls_806798_useDownloader.ts
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import { useTranscription } from "src/context/TranscriptionContext";
+import { useEditor } from "src/context/EditorContext";
+import useStyledHtmlExporter from "src/hooks/useStyledHtmlExporter";
+import useAxios from "src/hooks/useAxios";
+import { API_URL } from "src/utils/axios";
+
+interface Downloader {
+  generatePDF: () => Promise<Blob>;
+  isLoading: boolean;
+}
+
+const useDownloader = (): Downloader => {
+  const {
+    allHighlightColors,
+    fontColor,
+    fontSize,
+    fontStyle,
+    lineHeight,
+    wordSpacing,
+  } = useTranscription();
+  const { editorState } = useEditor();
+  const styledHtml = useStyledHtmlExporter(editorState, {
+    fontSize,
+    fontStyle,
+    wordSpacing,
+    lineHeight,
+    fontColor,
+    allHighlightColors,
+  });
+  // create a state to keep track of the loading state
+  const [isLoading, setIsLoading] = useState(false);
+  const axios = useAxios();
+  console.log(styledHtml)
+  const generatePDF = async (): Promise<Blob> => {
+    setIsLoading(true);
+    try {
+      const response = await axios.post(
+        `${API_URL}/generate-pdf/`,
+        {
+          raw_html: styledHtml,
+        },
+        {
+          responseType: "blob", // This tells Axios to expect a blob response
+        }
+      );
+
+      if (response.status >= 200 && response.status < 300) {
+        // The response is a Blob if the request succeeds
+        return response.data; // `response.data` is a Blob representing the PDF
+      } else {
+        console.error(
+          "Failed to generate PDF:",
+          response.status,
+          response.statusText
+        );
+        throw new Error("Failed to generate PDF");
+      }
+    } catch (error) {
+      console.error("An error occurred while generating PDF:", error);
+      throw error;
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return { generatePDF, isLoading };
+};
+
+export default useDownloader;
