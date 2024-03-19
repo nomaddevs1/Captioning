1c4da00ed80dcceb116b01c51a194e3556253f6e
Bell, Ryan <ryan.bell62101@gmail.com>
2024-03-18 20:44:43 -0500

    got vtt cue options working

--- server/models/web_vtt.py
+++ server/models/web_vtt.py
@@ -1,12 +1,23 @@
-from pydantic import BaseModel
-from typing import List
+from pydantic import BaseModel, constr
+from typing import List, Literal, Union
 
+
+class WebVTTCueOptions(BaseModel):
+    # vertical: Literal["lr", "rl"] = None
+    vertical: str = None
+    line: str = None
+    position: str = None
+    size: str = None
+    # align: Literal["start", "center", "end"] = None
+    align: str = None
+    
 
 class WebVTTCue(BaseModel):
     id: str = None
     start: float
     end: float
     payload: str
+    options: WebVTTCueOptions = None
 
 
 class WebVTTData(BaseModel):
--- server/tests/test_generate_vtt.py
+++ server/tests/test_generate_vtt.py
@@ -11,7 +11,7 @@ def basic_vtt_data() -> Generator[WebVTTData, None, None]:
     with open("tests/fixtures/vtt/basic_data.json", "r") as file:
         data = json.load(file)
         yield WebVTTData(cues=data)
-        
+
 
 @pytest.fixture
 def basic_vtt_data_with_ids() -> Generator[WebVTTData, None, None]:
@@ -20,9 +20,16 @@ def basic_vtt_data_with_ids() -> Generator[WebVTTData, None, None]:
         yield WebVTTData(cues=data)
 
 
+@pytest.fixture
+def advanced_vtt_data() -> Generator[WebVTTData, None, None]:
+    with open("tests/fixtures/vtt/advanced_data.json", "r") as file:
+        data = json.load(file)
+        yield WebVTTData(cues=data)
+
+
 def test_generate_vtt_with_basic_data(basic_vtt_data):
     vtt_str = generate_vtt(basic_vtt_data)
-    
+
     with open("tests/fixtures/vtt/basic_data.vtt", "r") as file:
         expected = file.read()
         assert vtt_str == expected
@@ -30,10 +37,18 @@ def test_generate_vtt_with_basic_data(basic_vtt_data):
 
 def test_generate_vtt_with_basic_data_with_ids(basic_vtt_data_with_ids):
     vtt_str = generate_vtt(basic_vtt_data_with_ids)
-    
+
     with open("tests/fixtures/vtt/basic_data_with_ids.vtt", "r") as file:
         expected = file.read()
-        print(expected)
         print(vtt_str)
+        print("-------------------------------")
         assert vtt_str == expected
+
+
+def test_generate_vtt_with_advanced_data(advanced_vtt_data):
+    vtt_str = generate_vtt(advanced_vtt_data)
+    
+    with open("tests/fixtures/vtt/advanced_data.vtt", "r") as file:
+        expected = file.read()
+        assert vtt_str == expected        
     
