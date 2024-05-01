f50638024d5c05961291f34b4efb8fe404cf810f
farouq <48164417+nomaddevs1@users.noreply.github.com>
2024-05-01 10:24:14 -0500

    Update axios.ts

--- transcript-client/src/utils/axios.ts
+++ transcript-client/src/utils/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { IS_PRODUCTION } from "./environment";
 
+
+const api_url = process.env.REACT_APP_API_URL ||  "https://api.transcribro.com"
 export const API_URL = IS_PRODUCTION
-  ? "https://api.transcribro.com"
+  ? api_url
   : "http://localhost:8000";
 
 export const AxiosPrivateClient = axios.create({
